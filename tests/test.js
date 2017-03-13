94aaad1c79ad134074d69dceb519d09de3d1f943
Forrest Desjardins <desjardinsfg@gmail.com>
2017-03-13 00:32:23 -0800

    add testable code for coverage

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,10 @@
 /* global describe, it */
 
 const expect = require('chai').expect
+const testable = [
+  require('../examples/dev-web-server.js'),
+  require('../examples/api/api.js')
+]
 
 describe('tests', () => {
   it('should run', done => {
